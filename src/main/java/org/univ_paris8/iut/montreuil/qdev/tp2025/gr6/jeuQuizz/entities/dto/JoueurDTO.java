0de7cfbab03c0d124959fd58ccbb4d396a313383
jkaku <kakujonathan128@gmail>
2025-03-13 13:59:24 +0100

    creation dto et packages

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr6/jeuQuizz/entities/dto/JoueurDTO.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr6/jeuQuizz/entities/dto/JoueurDTO.java
@@ -1,4 +1,25 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2025.gr6.jeuQuizz.entities.dto;
 
 public class JoueurDTO {
+    protected int compteur = 0;
+    private int id;
+    private String nom;
+    private String prenom;
+    private int score;
+    private String pseudo;
+    private int annéeNaissance;
+    private String centreInteret;
+    private Enum languePrefere;
+
+    public JoueurDTO(String nom,String prenom,String pseudo,int annéeNaissance,String centreInteret,Enum languePrefere) {
+        this.id = compteur;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.pseudo = pseudo;
+        this.annéeNaissance = annéeNaissance;
+        this.centreInteret = centreInteret;
+        this.languePrefere = languePrefere;
+        this.score = 0;
+        compteur++;
+    }
 }
--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr6/jeuQuizz/entities/dto/ScoreDTO.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr6/jeuQuizz/entities/dto/ScoreDTO.java
@@ -1,4 +1,11 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2025.gr6.jeuQuizz.entities.dto;
 
 public class ScoreDTO {
+    private int nombrePartie;
+    private int nombreBonneReponses;
+    private int moyenneG;
+    private int duréeG;
+    private int dernierePartiejouee;
+    public ScoreDTO() {}
+
 }
